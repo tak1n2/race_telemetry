c1da164b33c8bf0e49aa71cc78d61b86f300ba9f
Demydov Maksym <maksimdemidov05@gmail.com>
2025-09-04 19:57:17 +0300

    Changed names to snakecase

--- apps/core/models/__init__.py
+++ apps/core/models/__init__.py
@@ -2,4 +2,4 @@ from .driver import Driver
 from .lap import Lap
 from .car import Car
 from .track import Track
-from .telemetryPoint import TelemetryPoint
+from .telemetry_point import TelemetryPoint
