c99f2d44c8211a8571299f91fc651a6fdb250436
Demydov Maksym <maksimdemidov05@gmail.com>
2025-09-06 14:58:52 +0300

    class Team moved to its own file

--- apps/core/models/driver.py
+++ apps/core/models/driver.py
@@ -1,16 +1,5 @@
 from django.db import models
 
-class Team(models.Model):
-    name = models.CharField(max_length=100)
-    country = models.CharField(max_length=50)
-
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-
-    def __str__(self):
-        return f"{self.name} ({self.country})"
-
-
 class Driver(models.Model):
     # id
     first_name = models.CharField(max_length=50)
