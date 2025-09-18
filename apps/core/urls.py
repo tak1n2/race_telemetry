965b563ee4222b268b1d5dc5dd85df4c2f8feaa1
Demydov Maksym <maksimdemidov05@gmail.com>
2025-09-18 20:05:00 +0300

    Added drivers and teams pages with all data and functionality, css later

--- apps/core/urls.py
+++ apps/core/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from apps.core.views import about_view, welcome_view
+from apps.core.views import about_view, welcome_view, teams, drivers
 
 urlpatterns=[
     path('',welcome_view, name='welcome'),
-    path('drivers/',drivers_view, name='drivers'),
+    path('drivers/',drivers, name='drivers'),
+    path('teams/',teams, name='teams'),
+    path('about/',about_view, name='about'),
     path('tack_selection/',about_view, name='track_selection'),
 ]
--- apps/core/views.py
+++ apps/core/views.py
@@ -1,5 +1,9 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views.decorators.http import require_http_methods
+
+from apps.core.models import Driver, Team
+
 
 # Create your views here.
 def about_view(request):
@@ -12,24 +16,42 @@ def welcome_view(request):
 def drivers(request):
     #POST
     if request.method == "POST":
-        first_name = request.POST.get("first_name");
-        last_name = request.POST.get("last_name");
-        number = request.POST.get("number");
-        country = request.POST.get("country");
-        team = request.POST.get("team");
+        first_name = request.POST.get("first_name")
+        last_name = request.POST.get("last_name")
+        number = request.POST.get("number")
+        country = request.POST.get("country")
+        team_id = request.POST.get("team")
         #
-        Drivers.objects.create(
+        Driver.objects.create(
             first_name = first_name,
             last_name = last_name,
             number = number,
             country = country,
-            team = team
+            team=Team.objects.get(id=team_id)
         )
         return redirect("drivers")
     #GET
     context = {
-        "driver_list": Driver.object.all(),
-        "team": Team,
+        "driver_list": Driver.objects.all(),
+        "teams": Team.objects.all(),
     }
     return render(request, "core/pages/drivers.html", context)
+
+@require_http_methods(["GET","POST"])
+def teams(request):
+    #POST
+    if request.method == "POST":
+        name = request.POST.get("name")
+        country = request.POST.get("country")
+        #
+        Team.objects.create(
+            name = name,
+            country = country
+        )
+        return redirect("teams")
+    #GET
+    context = {
+        "team_list": Team.objects.all()
+    }
+    return render(request, "core/pages/teams.html", context)
         
